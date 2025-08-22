264ceb8def3414f520e935f727b9edffaed5add7
Hijo1918 <jonathanije3@gmail.com>
2025-08-22 04:38:06 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,28 @@
-@@ .. @@
- import React from 'react'
- import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
--import Layout from './components/Layout'
-+import Layout from './components/Layout.tsx'
- import Dashboard from './pages/Dashboard'
- import Trading from './pages/Trading'
- import Analytics from './pages/Analytics'
- import Settings from './pages/Settings'
-+import { Toaster } from 'react-hot-toast'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout.tsx';
+import Dashboard from './pages/Dashboard';
+import Trading from './pages/Trading';
+import Analytics from './pages/Analytics';
+import Settings from './pages/Settings';
+import { Toaster } from 'react-hot-toast';
 
- function App() {
-   return (
--    <Router>
--      <Layout>
--        <Routes>
--          <Route path="/" element={<Dashboard />} />
--          <Route path="/trading" element={<Trading />} />
--          <Route path="/analytics" element={<Analytics />} />
--          <Route path="/settings" element={<Settings />} />
--        </Routes>
--      </Layout>
--    </Router>
-+    <>
-+      <Router>
-+        <Layout>
-+          <Routes>
-+            <Route path="/" element={<Dashboard />} />
-+            <Route path="/trading" element={<Trading />} />
-+            <Route path="/analytics" element={<Analytics />} />
-+            <Route path="/settings" element={<Settings />} />
-+          </Routes>
-+        </Layout>
-+      </Router>
-+      <Toaster position="top-right" />
-+    </>
-   )
- }
+function App() {
+  return (
+    <>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/trading" element={<Trading />} />
+            <Route path="/analytics" element={<Analytics />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </Layout>
+      </Router>
+      <Toaster position="top-right" />
+    </>
+  );
+}
 
- export default App
+export default App;
