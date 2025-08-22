4a3e58d16e28f28c6a47517a704e0d9b697414f0
Hijo1918 <jonathanije3@gmail.com>
2025-08-22 07:11:06 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,16 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Example: split vendor code
+          react: ['react', 'react-dom'],
+        },
+      },
+      // You can also mark externals here if needed
+      // external: ['react-chartjs-2', 'chart.js']
+    },
+  },
 });
