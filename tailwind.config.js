ff80fdbcac66e82f97556eaef437ff28c016ca7b
Hijo1918 <jonathanije3@gmail.com>
2025-08-22 01:08:40 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",   // adjust if using React/Vue/Svelte
   ],
   theme: {
     extend: {},
